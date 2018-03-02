ccd279a160949120f7273fbdcbe76d681944471a
forafish <fora.luo@gmail.com>
2018-03-02 15:53:16 +0800

    remove copied code

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 if (!global._babelPolyfill) { require('babel-polyfill'); }
 
-const ramda = require('ramda');
-const func = require('./lib/function');
-const list = require('./lib/list');
-const logic = require('./lib/logic');
-const math = require('./lib/math');
-const object = require('./lib/object');
-const relation = require('./lib/relation');
-const string = require('./lib/string');
-const type = require('./lib/type');
-
-module.exports = Object.assign(ramda,
-  func, list, logic, math, object, relation, string, type
-);
+module.exports = {};
--- src/string.js
+++
@@ -1,7 +0,0 @@
-import R from 'ramda';
-
-/**
- * split path to get head or tail
- */
-export const splitHead = R.compose(R.head, R.split('.'));
-export const splitTail = R.compose(R.join('.'), R.tail, R.split('.'));
