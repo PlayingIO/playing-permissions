c354f476a3d21a564d8e4b2af73dc7acb684cc4d
forafish <fora.luo@gmail.com>
2018-03-02 17:52:02 +0800

    add toMongoQuery

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 if (!global._babelPolyfill) { require('babel-polyfill'); }
 
+module.exports = require('./lib/query');
 module.exports.Rule = require('./lib/rule');
 module.exports.RuleBuilder = require('./lib/builder');
---
+++ src/query.js
@@ -0,0 +1,36 @@
+import fp from 'mostly-func';
+
+export function rulesToQuery(rules, convert) {
+  const query = {};
+  const ignoreOperators = {};
+
+  for (let i = 0; i < rules.length; i++) {
+    const rule = rules[i];
+    const op = rule.inverted ? '$and' : '$or';
+
+    if (!rule.conditions) {
+      if (rule.inverted) {
+        return null;
+      }
+
+      if (query[op]) {
+        delete query[op];
+      }
+
+      ignoreOperators[op] = true;
+    } else if (!ignoreOperators.hasOwnProperty(op)) {
+      query[op] = query[op] || [];
+      query[op].push(convert(rule));
+    }
+  }
+
+  return rules.length > 0 ? query : null;
+}
+
+function convertRuleToQuery(rule) {
+  return rule.inverted ? { $nor: [rule.conditions] } : rule.conditions;
+}
+
+export function toMongoQuery(rules) {
+  return rulesToQuery(rules, convertRuleToQuery);
+}
