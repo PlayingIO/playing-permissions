cc84163e95696c0bf076b76397cb1245cd4b67c6
forafish <fora.luo@gmail.com>
2018-04-17 17:26:55 +0800

    add authorize hook

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import Aces from './aces';
 import AceBuilder from './builder';
 import { rulesToQuery, toMongoQuery } from './query';
 import Rule from './rule';
+import authorize from './hooks/authorize';
 
 export {
   Aces,
   AceBuilder,
+  authorize,
   rulesToQuery,
   toMongoQuery,
   Rule
-}
+};
